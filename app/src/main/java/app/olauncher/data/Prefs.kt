08019143d0841dd29b59c41a2a6df10a1a2e210a
sorathpanzer <sorathpanzer@gmail.com>
2021-06-04 13:51:58 +0100

    Default apps changed to 8

--- app/src/main/java/app/olauncher/data/Prefs.kt
+++ app/src/main/java/app/olauncher/data/Prefs.kt
@@ -101,7 +101,7 @@ class Prefs(context: Context) {
         set(value) = prefs.edit().putString(WALLPAPER_UPDATED_DAY, value).apply()
 
     var homeAppsNum: Int
-        get() = prefs.getInt(HOME_APPS_NUM, 4)
+        get() = prefs.getInt(HOME_APPS_NUM, 8)
         set(value) = prefs.edit().putInt(HOME_APPS_NUM, value).apply()
 
     var homeAlignment: Int
